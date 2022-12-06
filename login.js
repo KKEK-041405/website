d05867c4aa16369a365345bf6ebf154ad417119b
komalkonatham <komaleshwarakumarkonatham@gmail.com>
2022-12-06 10:09:05 +0530

    login with username and password

---
+++ login.js
@@ -0,0 +1,7 @@
+function login() {
+    var x = document.getElementById("username").value;
+    var y = document.getElementById("password").value;
+    if(x == "KKEK" && y=="KKEK"){
+        window.location.replace("dashboard.html");
+    }    
+}
