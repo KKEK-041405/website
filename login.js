7b44f4e9958f47d5b12e34cbd12b6eb92a57b017
komalkonatham <komaleshwarakumarkonatham@gmail.com>
2022-12-06 12:35:00 +0530

    test

--- login.js
+++ login.js
@@ -1,7 +1,17 @@
-function login() {
-    var x = document.getElementById("username").value;
-    var y = document.getElementById("password").value;
+function login(){
     if(x == "KKEK" && y=="KKEK"){
-        window.location.replace("dashboard.html");
-    }    
+    window.location.replace("dashboard.html");
+}    
+}
+function VALID() {
+var x = document.getElementById("username").value;
+var y = document.getElementById("password").value;
+//alert('PLS ENTER USERNAME');
+if(x == ""){
+    alert('PLS ENTER USERNAME');
+}
+if(y == ""){
+    alert('PLS ENTER PASSWORD ');
+}
+login();
 }
